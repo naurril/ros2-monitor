0f8b5869a73e58151fa6bebae2f49ff9b2d585ac
Naurril <naurril@gmail.com>
2023-12-15 11:42:57 +0800

    acce/speed

--- monitor.js
+++ monitor.js
@@ -26,94 +26,150 @@ ros.on('close', function () {
 });
 
 // Create a connection to the rosbridge WebSocket server.
-ros.connect('ws://localhost:19090');
+ros.connect('ws://localhost:9090');
 
-const ctx = document.getElementById('chart1');
+function draw(chartid, topic1, topic2) {
+    const ctx = document.getElementById(chartid);
 
-let labels = []
-let data1 = []
-let data2 = []
-const data = {
-    labels: labels,
-    datasets: [{
-        label: 'steering angle cmd',
-        data: data1,
-        // fill: false,
-        // borderColor: 'rgb(0, 192, 192)',
-        backgroundColor: 'rgb(0, 0, 192)',
-        // tension: 0.1
-    },
-    {
-        label: 'steering angle status',
-        data: data2,
-        // fill: false,
-        // borderColor: 'rgb(0, 192, 192)',
-        backgroundColor: 'rgb(192, 0, 0)',
-        // tension: 0.1
-    }
-]
-};
-
-const config = {
-    type: 'scatter',
-    data: data,
-    options: {
-        scales: {
-        //   x: {
-        //     type: 'linear',
-        //     position: 'bottom'
-        //   },
+    let labels = []
+    let data1 = [{ x: 0, y: 0 }]
+    let data2 = [{ x: 0, y: 0 }]
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: topic1.label,
+            data: data1,
+            // fill: false,
+            // borderColor: 'rgb(0, 192, 192)',
+            backgroundColor: 'rgb(0, 0, 192)',
+            // tension: 0.1
         },
-        animation: false
-      }
-};
+        {
+            label: topic2.label,
+            data: data2,
+            // fill: false,
+            // borderColor: 'rgb(0, 192, 192)',
+            backgroundColor: 'rgb(192, 0, 0)',
+            // tension: 0.1
+        }
+        ]
+    };
 
-let chart = new Chart(ctx, config);
+    const config = {
+        type: 'scatter',
+        data: data,
+        options: {
+            scales: {
+                //   x: {
+                //     type: 'linear',
+                //     position: 'bottom'
+                //   },
+            },
+            animation: false
+        }
+    };
 
-// Like when publishing a topic, we first create a Topic object with details of the topic's name
-// and message type. Note that we can call publish or subscribe on the same topic object.
-var listener = new ROSLIB.Topic({
-    ros: ros,
-    name: '/control/command/control_cmd',
-    messageType: 'autoware_auto_control_msgs/msg/AckermannControlCommand'
-});
+    let chart = new Chart(ctx, config);
 
-// Then we add a callback to be called every time a message is published on this topic.
-listener.subscribe(function (message) {
-    //console.log(listener.name, message);
-    let ts = message.stamp.sec + message.stamp.nanosec * 1e-9;
+    // Like when publishing a topic, we first create a Topic object with details of the topic's name
+    // and message type. Note that we can call publish or subscribe on the same topic object.
+    var listener = new ROSLIB.Topic(topic1.topic);
 
-    if (data1.length > 500) {
-        // listener.unsubscribe();
-        data1.shift()
-    }
+    // Then we add a callback to be called every time a message is published on this topic.
+    listener.subscribe(function (message) {
+        //console.log(listener.name, message);
+        let ts = Date.now() //message.stamp.sec + message.stamp.nanosec * 1e-9;
 
-    // labels.push(Math.round(ts*10))
-    data1.push({x:ts, y:message.lateral.steering_tire_angle})
-    chart.update()
-    // If desired, we can unsubscribe from the topic as well.
-    
-});
+        if (ts - data1[0].x > 20000) {
+            // listener.unsubscribe();
+            data1.shift()
+        }
+
+        // labels.push(Math.round(ts*10))
+        data1.push({ x: ts, y: topic1.getvalue(message) })
+        chart.update()
+        // If desired, we can unsubscribe from the topic as well.
+
+    });
 
 
-var listener2 = new ROSLIB.Topic({
-    ros: ros,
-    name: '/vehicle/status/steering_status',
-    messageType: 'autoware_auto_vehicle_msgs/msg/SteeringReport'
-});
+    var listener2 = new ROSLIB.Topic(topic2.topic);
 
-listener2.subscribe(function (message) {
-    //console.log(listener.name, message);
-    let ts = message.stamp.sec + message.stamp.nanosec * 1e-9;
+    listener2.subscribe(function (message) {
+        //console.log(listener.name, message);
+        let ts = Date.now()//message.stamp.sec + message.stamp.nanosec * 1e-9;
 
-    if (data2.length > 500) {
-        // listener.unsubscribe();
-        data2.shift()
-    }
+        if ((data2.length > 10) &&  (ts - data2[0].x > 20000)){
+            // listener.unsubscribe();
+            data2.shift()
+        }
 
-    // labels.push(Math.round(ts*10))
-    data2.push({x:ts, y: message.steering_tire_angle})
-    chart.update()
-    // If desired, we can unsubscribe from the topic as well.
-    
-})
+        // labels.push(Math.round(ts*10))
+        data2.push({ x: ts, y: topic2.getvalue(message) })
+        chart.update()
+        // If desired, we can unsubscribe from the topic as well.
+
+    })
+
+}
+
+draw('chart1', {
+    topic: {
+        ros: ros,
+        name: '/control/command/control_cmd',
+        messageType: 'autoware_auto_control_msgs/msg/AckermannControlCommand',
+    },
+
+    getvalue: message => message.lateral.steering_tire_angle,
+    label: "steering cmd"
+},
+    {
+        topic: {
+            ros: ros,
+            name: '/vehicle/status/steering_status',
+            messageType: 'autoware_auto_vehicle_msgs/msg/SteeringReport',
+        },
+        getvalue: message => message.steering_tire_angle,
+        label: "steering status"
+    })
+
+
+draw('chart2', {
+    topic: {
+        ros: ros,
+        name: '/control/command/control_cmd',
+        messageType: 'autoware_auto_control_msgs/msg/AckermannControlCommand',
+    },
+
+    getvalue: message => message.longitudinal.speed,
+    label: "speed cmd"
+},
+    {
+        topic: {
+            ros: ros,
+            name: '/vehicle/status/velocity_status',
+            messageType: 'autoware_auto_vehicle_msgs/msg/VelocityReport',
+        },
+        getvalue: message => message.longitudinal_velocity,
+        label: "speed status"
+    })
+
+draw('chart3', {
+    topic: {
+        ros: ros,
+        name: '/control/command/control_cmd',
+        messageType: 'autoware_auto_control_msgs/msg/AckermannControlCommand',
+    },
+
+    getvalue: message => message.longitudinal.acceleration,
+    label: "accel cmd"
+},
+    {
+        topic: {
+            ros: ros,
+            name: '/localization/acceleration',
+            messageType: 'geometry_msgs/msg/AccelWithCovarianceStamped',
+        },
+        getvalue: message => message.accel.accel.linear.x,
+        label: "accel status"
+    })
